1fc9994382272320b60ff493331b0f6ef98322ee
IHCantabraba <inigocanta@gmail.com>
2024-04-15 20:22:33 +0200

    RPS

---
+++ src/components/utils/RPS/dialogBoard/dialogBoard.js
@@ -0,0 +1,60 @@
+import { createDiv } from '../../TicTacToe/CreateDiv'
+import './dialogBoard.css'
+
+export const dialogBoard = () => {
+  /* get content element to insert board */
+  const content = document.querySelector('.content')
+  /* get options to insert board Before */
+  const options = document.querySelector('.options')
+
+  /* create dialogBoard Section */
+  const dialogBoard = document.createElement('section')
+  dialogBoard.classList.add('dialogBoard')
+
+  /* create dialogBoard title */
+  const boardTitle = document.createElement('h1')
+  boardTitle.classList.add('sectionTitle')
+  boardTitle.textContent = 'Take a risk and Play!'
+  /* selections div */
+  const batleSelection = createDiv('batleSelection')
+  /* create dialogBoard title */
+  const userArticle = document.createElement('article')
+  userArticle.classList.add('userArticle')
+  const userSelection = document.createElement('h2')
+  userSelection.classList.add('userSelection')
+  userSelection.textContent = 'You have chosen... '
+  const userSelecImg = document.createElement('img')
+  userSelecImg.classList.add('userSelecImg')
+
+  /* create dialogBoard title */
+  const computerArticle = document.createElement('article')
+  computerArticle.classList.add('computerArticle')
+  const computerSelection = document.createElement('h2')
+  computerSelection.classList.add('computerSelection')
+  computerSelection.textContent = 'Computer has chosen...'
+
+  /* adding imag element for chosen option */
+  const computerSelecImg = document.createElement('img')
+  computerSelecImg.classList.add('computerSelecImg')
+
+  /* adding elements to articles */
+  /* user relative */
+  userArticle.append(userSelection)
+  userArticle.append(userSelecImg)
+
+  /* computer relative */
+  computerArticle.append(computerSelection)
+  computerArticle.append(computerSelecImg)
+  /* adding element to batle selection div */
+  batleSelection.append(userArticle)
+  batleSelection.append(computerArticle)
+  /* adding elements to section */
+  dialogBoard.append(boardTitle)
+  dialogBoard.append(batleSelection)
+
+  if (options) {
+    content.insertBefore(dialogBoard, options)
+  } else {
+    content.append(dialogBoard)
+  }
+}
--- src/pages/RPS/rps.js
+++ src/pages/RPS/rps.js
@@ -1,5 +1,6 @@
 import { deleteTimers } from '../../components/utils/BrainGame/functions/funtions'
-import { showBatle } from '../../components/utils/RPS/InitGame/initRPS'
+import { dialogBoard } from '../../components/utils/RPS/dialogBoard/dialogBoard'
+// import { showBatle } from '../../components/utils/RPS/InitGame/initRPS'
 import { insertGameOptions } from '../../components/utils/RPS/gameOptions/gameOptions'
 import { initResultsRPS } from '../../components/utils/RPS/initResultDiv/RPSresultDiv'
 import './rps.css'
@@ -15,7 +16,7 @@ export const ActivateRPSGame = (textContent) => {
     footerP.style.color = 'white'
     initResultsRPS()
     insertGameOptions()
-    showBatle()
+    dialogBoard()
 
     /* initOptionSelection() */
   })
